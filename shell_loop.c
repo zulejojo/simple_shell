12e506f6d0086259f8905e54b0656d57519d49bc
zulejojo <zulejojo@gmail.com>
2023-02-26 22:12:34 -0800

    simple shell

--- shell_loop.c
+++ shell_loop.c
@@ -41,14 +41,13 @@ exit(info->err_num);
 return (builtin_ret);
 }
 /**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
- */
+*find_builtin - finds a builtin command
+*@info: the parameter & return info struct
+** Return: -1 if builtin not found,
+*0 if builtin executed successfully,
+*1 if builtin found but not successful,
+*2 if builtin signals exit()
+*/
 int find_builtin(info_t *info)
 {
 int i, built_in_ret = -1;
